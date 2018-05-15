2b3a12f9fe53e38f69275a14da4dcebbc16b0e78
kation wang <wangchangchao@bearead.cn>
2018-05-15 14:04:59 +0800

    no message

---
+++ src/main/java/com/wccwin/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.wccwin.eureka;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
